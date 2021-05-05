b878fdbbfde52e4150b6bea82892b55ebd7caeba
Ahmedsabry11 <75908511+Ahmedsabry11@users.noreply.github.com>
2021-05-05 13:23:13 +0200

    Priority Queue

--- DT_MARS-EXPLORE/Node.h
+++ DT_MARS-EXPLORE/Node.h
@@ -7,35 +7,40 @@ class Node
 private :
 	T item; // A data item
 	Node<T>* next; // Pointer to next node
+	double priority;
 public :
 	Node();
-	Node( const T & r_Item);	
-	Node( const T & r_Item, Node<T>* nextNodePtr);
+	Node( const T & r_Item, double p = 0);
+	Node( const T & r_Item, Node<T>* nextNodePtr, double p = 0);
 	void setItem( const T & r_Item);
 	void setNext(Node<T>* nextNodePtr);
 	T getItem() const ;
+	double getpriority() const;
 	Node<T>* getNext() const ;
 }; // end Node
 #endif
 
 template < typename T>
-Node<T>::Node() 
+Node<T>::Node()
 {
 	next = nullptr;
+	priority = 0;
 } 
 
 template < typename T>
-Node<T>::Node( const T& r_Item)
+Node<T>::Node( const T& r_Item,double p)
 {
 	item = r_Item;
 	next = nullptr;
+	priority = p;
 } 
 
 template < typename T>
-Node<T>::Node( const T& r_Item, Node<T>* nextNodePtr)
+Node<T>::Node( const T& r_Item, Node<T>* nextNodePtr,double p)
 {
 	item = r_Item;
 	next = nextNodePtr;
+	priority = p;
 }
 template < typename T>
 void Node<T>::setItem( const T& r_Item)
@@ -54,6 +59,12 @@ T Node<T>::getItem() const
 {
 	return item;
 } 
+
+template < typename T>
+double Node<T>::getpriority() const
+{
+	return priority;
+}
 
 template < typename T>
 Node<T>* Node<T>::getNext() const
---
+++ DT_MARS-EXPLORE/PriorityQueue.h
@@ -0,0 +1,131 @@
+#pragma once
+#ifndef PRIORITY_QUEUE_
+#define PRIORITY_QUEUE_
+//#define LINKED_QUEUE_
+
+#include "Node.h"
+#include "QueueADT.h"
+#include <vector>
+using namespace std;
+
+
+template <typename T>
+class PriorityQueue :public PriorityQueueADT<T>
+{
+private:
+	Node<T>* backPtr;
+	Node<T>* frontPtr;
+public:
+	PriorityQueue() 
+	{
+		backPtr = nullptr;
+		frontPtr = nullptr;
+	}
+	bool isEmpty() const
+	{
+		return (frontPtr == nullptr);
+	}
+
+
+	bool enqueue(const T& newEntry, double priority) 
+	{
+		Node<T>* newNodePtr = new Node<T>(newEntry, priority);
+		// Insert the new node
+		if (isEmpty()) {	//special case if this is the first node to insert
+			frontPtr = newNodePtr;
+			backPtr = newNodePtr;
+			return true;
+		}                   // The queue is empty
+		else if (priority > frontPtr->getpriority())
+		{
+			newNodePtr->setNext(frontPtr);
+			frontPtr = newNodePtr;
+			return true;
+		}
+		else
+		{
+			Node<T>* temp = frontPtr;
+			while (temp->getNext())
+			{
+				if (temp->getNext()->getpriority() < priority)
+				{
+					newNodePtr->setNext(temp->getNext());
+					temp->setNext(newNodePtr);
+					return true;
+				}
+				temp = temp->getNext();
+			}
+			temp->setNext(newNodePtr);
+			backPtr = newNodePtr; // New node is the last node now
+		}
+		return true;
+		
+	}
+
+
+
+	bool dequeue(T& frntEntry)
+	{
+		if(isEmpty())
+		return false;
+
+	Node<T>* nodeToDeletePtr = frontPtr;
+	frntEntry = frontPtr->getItem();
+	frontPtr = frontPtr->getNext();
+	// Queue is not empty; remove front
+	if (nodeToDeletePtr == backPtr)	 // Special case: last node in the queue
+		backPtr = nullptr ;	
+		
+	// Free memory reserved for the dequeued node
+	delete nodeToDeletePtr;
+
+	return true;
+	}
+
+	bool peek(T& frntEntry)  const
+	{
+		if (isEmpty())
+			return false;
+
+		frntEntry = frontPtr->getItem();
+		return true;
+	}
+	~PriorityQueue() 
+	{
+		T temp;
+
+		//Free (Dequeue) all nodes in the queue
+		while (dequeue(temp));
+	}
+
+	//copy constructor
+	PriorityQueue(const PriorityQueue<T>& LQ)
+	{
+		Node<T>* NodePtr = LQ.frontPtr;
+		if (!NodePtr) //LQ is empty
+		{
+			frontPtr = backPtr = nullptr;
+			return;
+		}
+
+		//insert the first node
+		Node<T>* ptr = new Node<T>(NodePtr->getItem());
+		frontPtr = backPtr = ptr;
+		NodePtr = NodePtr->getNext();
+
+		//insert remaining nodes
+		while (NodePtr)
+		{
+			Node<T>* ptr = new Node<T>(NodePtr->getItem());
+			backPtr->setNext(ptr);
+			backPtr = ptr;
+			NodePtr = NodePtr->getNext();
+		}
+	}
+
+};
+
+
+
+
+#endif
