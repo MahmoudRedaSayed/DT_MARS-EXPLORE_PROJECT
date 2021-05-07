c125ef497ae44ac425d0b9ca4328b4972f65989c
Mahmoud Reda <anamahmoud10203040@gmail.com>
2021-05-07 21:31:18 +0200

    Error is solved

---
+++ DT_MARS-EXPLORE/Rover.cpp
@@ -0,0 +1,8 @@
+#include"Rover.h"
+int Rover::ID_Count = 0;
+int Rover::P_Rover_Count = 0;
+int Rover::Check_PR = 5;
+int Rover::E_Rover_Count = 0;
+int Rover::Check_ER = 5;
+int Rover::M_Rover_Count = 0;
+int Rover::Check_MR = 5;
--- DT_MARS-EXPLORE/Rover.h
+++ DT_MARS-EXPLORE/Rover.h
@@ -92,11 +92,5 @@ public:
 
 };
 //static members initializers
-int Rover::ID_Count = 0;
-int Rover::P_Rover_Count = 0;
-int Rover::Check_PR = 5;
-int Rover::E_Rover_Count = 0;
-int Rover::Check_ER = 5;
-int Rover::M_Rover_Count = 0;
-int Rover::Check_MR = 5;
+
 
