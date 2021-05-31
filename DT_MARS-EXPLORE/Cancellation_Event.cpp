3864ba912d2d0bfea76cc7c81e05e9d8ea700daf
Mahmoud Reda <anamahmoud10203040@gmail.com>
2021-05-31 18:05:20 +0200

    first test

--- DT_MARS-EXPLORE/Cancellation_Event.cpp
+++ DT_MARS-EXPLORE/Cancellation_Event.cpp
@@ -9,21 +9,37 @@ Cancellation_Event::Cancellation_Event(int id, int ED)
 void Cancellation_Event::Execute(PriorityQueue<Mission*>& Eme_Missions, LinkedQueue<Mission*>& Pol_Missions, LinkedQueue<Mission*>& Mou_Missions)
 {
 	Mission* ITERATOR = NULL;
+	Mission* ITERATOR2 = NULL;
 	Mission* TOP=NULL;
 	Mou_Missions.peek(TOP);
+	if (TOP == nullptr)
+	{
+		return;
+	}
+	if (TOP)
+	{
+		if (TOP->Get_ID() == Get_Mission_ID())
+		{
+			Mou_Missions.dequeue(TOP);
+			delete TOP;
+			return;
+		}
+	}
 	while (ITERATOR != TOP)
 	{
 		Mou_Missions.dequeue(ITERATOR);
 		if (ITERATOR->Get_ID() == Get_Mission_ID())
-			break;                                  //And the value of the mission will be stored in pointer iterator 
+		{
+			ITERATOR2 = ITERATOR;
+		}                                //And the value of the mission will be stored in pointer iterator 
 		else
 		{
 			Mou_Missions.enqueue(ITERATOR);
 		}
 	}
-	if(ITERATOR)
+	if(ITERATOR2)
 	{
-		delete ITERATOR;
+		delete ITERATOR2;
 	}
 	
 }
--- DT_MARS-EXPLORE/Promotion_Event.cpp
+++ DT_MARS-EXPLORE/Promotion_Event.cpp
@@ -13,23 +13,45 @@ Promotion_Event::~Promotion_Event()
 void Promotion_Event::Execute(PriorityQueue<Mission*>& Eme_Missions, LinkedQueue<Mission*>& Pol_Missions, LinkedQueue<Mission*>& Mou_Missions)
 {
 	Mission* ITERATOR=NULL;
+	Mission* ITERATOR2 = NULL;
 	Mission* TOP=NULL;
-	Mou_Missions.peek(TOP);
+	Mou_Missions.peek(TOP); 
+	if (TOP == nullptr)
+	{
+		return;
+	}
+	if (TOP)
+	{
+		if (TOP->Get_ID() == Get_Mission_ID())
+		{
+			Mou_Missions.dequeue(TOP);
+			Mission* Promoted_Mission = new Mission(TOP->Get_TLOC(), TOP->Get_MDUR(), TOP->Get_SIG(), TOP->Get_FD(), TOP->Get_ID(), Emergency);
+			delete TOP;
+			Eme_Missions.enqueue(Promoted_Mission, Promoted_Mission->Calculate_priority()); //Add the Mission to the emergency missions queue 
+			Mission::NumOfEMissions++;
+			Mission::NumOfNoNAutoPMissions++;
+			Mission::NumOfMMissions--;
+			return;
+		}
+	}
+
 	while (ITERATOR != TOP)
 	{
 		Mou_Missions.dequeue(ITERATOR);
 		if (ITERATOR->Get_ID() == Get_Mission_ID())
-			break;                                  //And the value of the mission will be stored in pointer iterator 
+		{
+			ITERATOR2 = ITERATOR;
+		}                                //And the value of the mission will be stored in pointer iterator2 
 		else
 		{
 			Mou_Missions.enqueue(ITERATOR);
 		}
 	}
 	//Setting the data to the new mission rather than use a copy constructor
-	if (ITERATOR) 
+	if (ITERATOR2) 
 	{
-		Mission* Promoted_Mission = new Mission(ITERATOR->Get_TLOC(),ITERATOR->Get_MDUR(), ITERATOR ->Get_SIG(),ITERATOR->Get_FD(), ITERATOR->Get_ID(), Emergency);
-		delete ITERATOR;
+		Mission* Promoted_Mission = new Mission(ITERATOR2->Get_TLOC(),ITERATOR2->Get_MDUR(), ITERATOR2 ->Get_SIG(),ITERATOR2->Get_FD(), ITERATOR2->Get_ID(), Emergency);
+		delete ITERATOR2;
 		Eme_Missions.enqueue(Promoted_Mission, Promoted_Mission->Calculate_priority()); //Add the Mission to the emergency missions queue 
 		Mission::NumOfEMissions++;
 		Mission::NumOfNoNAutoPMissions++;
